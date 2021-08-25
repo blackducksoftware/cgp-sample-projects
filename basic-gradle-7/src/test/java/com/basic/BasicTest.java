b981f2b2d49b2de9e6c3b8de2921001434b0ab36
Dana Maxfield <danam@synopsys.com>
2021-08-25 14:58:05 -0400

    Additional configuration

---
+++ basic-gradle-7/src/test/java/com/basic/BasicTest.java
@@ -0,0 +1,11 @@
+package com.basic;
+
+import org.junit.jupiter.api.Test;
+
+public class BasicTest {
+
+    @Test
+    public void dummyTest() {
+        System.out.println("I'm a test");
+    }
+}
